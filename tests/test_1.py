a387a24adea4408a0db1adcde2ab96264a0697fb
igorfltv <38211024+igorfltv@users.noreply.github.com>
2022-06-16 15:14:00 +0300

    Tests

---
+++ tests/test_1.py
@@ -0,0 +1,19 @@
+import unittest
+#Test cases to test Calulator methods
+#You always create  a child class derived from unittest.TestCase
+class TestServer(unittest.TestCase):
+  #setUp method is overridden from the parent class TestCase
+  def setUp(self):
+    pass
+  #Each test method starts with the keyword test_
+  def test_add(self):
+    pass
+  def test_subtract(self):
+    pass
+  def test_multiply(self):
+    pass
+  def test_divide(self):
+    pass
+# Executing the tests in the above test case class
+if __name__ == "__main__":
+  unittest.main()
